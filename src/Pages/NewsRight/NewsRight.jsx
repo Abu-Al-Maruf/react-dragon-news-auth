e170808a8b7358e39535b10934a7625186a819c9
Abu Al Maruf <mdmaruf55667711@gmail.com>
2023-10-29 21:05:21 +0600

    right side done

--- src/Pages/NewsRight/NewsRight.jsx
+++ src/Pages/NewsRight/NewsRight.jsx
@@ -1,10 +1,93 @@
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import qZone1 from "../../assets/qZone1.png";
+import qZone2 from "../../assets/qZone2.png";
+import qZone3 from "../../assets/qZone3.png";
+import bgImg from "../../assets/bg.png";
 
 const NewsRight = () => {
-    return (
-        <div>
-            <h2>News Right</h2>
-        </div>
-    );
+
+    const bgImage = {
+        backgroundImage:  `url(${bgImg})`
+    }
+
+
+  return (
+    <div>
+      {/* login with  */}
+      <div>
+        <h2 className="text-2xl font-semibold">Login With</h2>
+
+        <button className="btn btn-outline btn-info w-full mt-5 flex items-center">
+          <FaGoogle />
+          Google
+        </button>
+        <button className="btn btn-outline w-full mt-2 flex items-center">
+          <FaGithub />
+          Github
+        </button>
+      </div>
+
+      {/* find us on  */}
+      <div className="mt-10">
+        <h2 className="text-2xl font-semibold">Find Us On</h2>
+
+        <a
+          className="border flex items-center gap-3 font-medium text-[#706F6F] p-4 rounded-t border-b-0 mt-6"
+          href=""
+        >
+          <span className="bg-[#F3F3F3] p-3 rounded-full">
+            <FaFacebookF className="text-[#3B599C]" />
+          </span>{" "}
+          Facebook
+        </a>
+        <a
+          className="border flex items-center gap-3 font-medium text-[#706F6F] p-4 border-b-0"
+          href=""
+        >
+          <span className="bg-[#F3F3F3] p-3 rounded-full">
+            <FaTwitter className="text-[#58A7DE]" />
+          </span>{" "}
+          Twitter
+        </a>
+        <a
+          className="border flex items-center gap-3 font-medium text-[#706F6F] p-4 rounded-b "
+          href=""
+        >
+          <span className="bg-[#F3F3F3] p-3 rounded-full">
+            <FaInstagram className="text-[#bb3071]" />
+          </span>{" "}
+          Intagram
+        </a>
+      </div>
+
+      {/* Q-zone  */}
+      <div className="bg-[#F3F3F3] mt-4 p-4">
+        <h2 className="text-2xl font-semibold">Q-Zone</h2>
+
+        <img src={qZone1} alt="" />
+        <img src={qZone2} alt="" />
+        <img src={qZone3} alt="" />
+      </div>
+
+      {/* learn more  */}
+
+      <div style={bgImage} className="bg-gray-400 py-14 mt-5 px-10 text-center">
+        <h2 className="text-3xl text-white font-semibold">
+          Create an Amazing Newspaper
+        </h2>
+        <p className="text-base text-white font-normal mt-5">
+        Discover thousands of options, easy to customize layouts, one-click to import demo and much more.
+        </p>
+        <button className="btn btn-error bg-[#D72050] border-0 mt-8 rounded-none text-white">Learn More</button>
+      </div>
+    </div>
+  );
 };
 
 export default NewsRight;
--- src/Pages/Shared/Home/Home.jsx
+++ src/Pages/Shared/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Header></Header>
       <Navbar></Navbar>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-14">
         <div>
           <NewsLeft></NewsLeft>
         </div>
